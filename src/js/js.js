b1313911f4cbe158a3f60bc3e4c56c51a5804895
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2024-03-27 01:04:49 +0300

    deg

--- src/js/js.js
+++ src/js/js.js
@@ -16,4 +16,38 @@ https://github.com/htmlpluscss/
 
 	});
 
+	const anim = ( btn, deg, spin = 1 ) => {
+
+		deg = spin > 0 ? deg + 1 : deg - 3;
+
+		btn.style.setProperty('--deg', deg + 'deg');
+
+		window.requestAnimationFrame( () => {
+
+			if ( deg > 175 && deg < 200 ) {
+
+				anim(btn,deg,spin);
+
+			}
+
+		});
+
+	}
+
+	[...document.querySelectorAll('.btn')].forEach( btn => {
+
+		btn.addEventListener('mouseenter', ()=>{
+
+			anim(btn,200,-1);
+
+		});
+
+		btn.addEventListener('mouseleave', ()=>{
+
+			anim(btn,175);
+
+		});
+
+	});
+
 })();
