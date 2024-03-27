70e10d699791aac90573888e61d3cdb1bcebb5b7
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2024-03-27 11:44:23 +0300

    pages

--- src/js/menu.js
+++ src/js/menu.js
@@ -6,7 +6,7 @@
 
 			const btn = event.target.closest('.menu__btn');
 
-			[...btns].forEach( _btn => _btn.classList.toggle('is-open', btn !== null && _btn === btn ));
+			[...btns].forEach( _btn => _btn.classList.toggle('is-open', btn !== null && _btn === btn && btn.classList.contains('is-open') === false ) );
 
 			document.body.classList.toggle('is-open-menu', [...btns].some( btn => btn.classList.contains('is-open') ) );
 
