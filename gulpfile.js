9df284c90f043b854d51cb31da2588ce960b733d
mixkovrigin <159890280+mixkovrigin@users.noreply.github.com>
2024-04-25 21:11:45 +0300

    flag

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ const w3cjs            = require('gulp-w3cjs');
 
 const site             = 'Fenilux';
 const domain           = 'fenilux.io';
-const langs            = ['en','pt','ru','de'];
+const langs            = ['en','es','ar','pt','ru','fr','de'];
 const translate        = require('./translations');
 
 // если нет перевода
@@ -87,10 +87,7 @@ const html = (files, since = {}) => {
 					},
 					path: 'src/'
 				}))
-				.pipe(w3cjs({
-					url : 'https://validator.w3.org/nu/'
-				}))
-				.pipe(w3cjs.reporter())
+
 				.pipe(replace('css/styles.css', 'css/styles.min.css?' + Date.now()))
 				.pipe(replace('js/scripts.js', 'js/scripts.min.js?' + Date.now()))
 				.pipe(gulp.dest('build' + folder ))
